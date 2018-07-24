de0e90607c2228524b4510eab60469f732072ed2
ssengel <sametsengel@gmail.com>
2018-07-24 19:37:01 +0300

    version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router'
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WallComponent } from './components/wall/wall.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { CompaniesComponent } from './components/companies/companies.component';
+import { StoriesComponent } from './components/stories/stories.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+const routerConfig: Route[] = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {path: 'companies', component: CompaniesComponent},
+      {path: 'stories', component: StoriesComponent}
+    ]
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WallComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    CompaniesComponent,
+    StoriesComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routerConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
